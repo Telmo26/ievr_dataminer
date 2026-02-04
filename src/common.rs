e912303f6c1aef64ce9cd5906d0c2168ad56e8b2
Tadros Eliott <53917558+Telmo26@users.noreply.github.com>
2026-02-04 14:39:18 +0100

    Added text extraction Text is now correctly extracted into several databases in parallel. I also added a Python script to test if every character has their name in the files

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, path::Path};
 
-use ievr_cfg_bin_editor_core::{Database, parse_database};
+use ievr_cfg_bin_editor_core::{Database, Value, parse_database};
 use memmap2::Mmap;
 
 pub fn parse_gamefile(file_path: &Path) -> Option<Database> {
@@ -9,4 +9,25 @@ pub fn parse_gamefile(file_path: &Path) -> Option<Database> {
     let mmap = unsafe { Mmap::map(&file).unwrap() };
 
     parse_database(&mmap).ok()
+}
+
+pub fn parse_int_value(value: &Value) -> i32 {
+    match value {
+        Value::Int(v) => *v,
+        _ => unreachable!()
+    }
+}
+
+pub fn parse_byte_value(value: &Value) -> u8 {
+    match value {
+        Value::Byte(v) => *v,
+        _ => unreachable!()
+    }
+}
+
+pub fn parse_string_value(value: &Value) -> String {
+    match value {
+        Value::String(s) => s.clone(),
+        _ => panic!("Encountered {:?}", value)
+    }
 }
